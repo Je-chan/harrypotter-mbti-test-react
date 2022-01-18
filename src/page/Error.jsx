87bf3eb0e6c721acfd23bc175ef96a30a911afe5
박예찬 <jkyyc3@gmail.com>
2022-01-18 21:21:44 +0900

    UPDATE: Error 버튼 스타일 수정

--- src/page/Error.jsx
+++ src/page/Error.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
-import Button from '../styles/Button'
+import { GlobalButton } from '../styles/Button'
 import { Link } from 'react-router-dom'
 
-export const Wrapper = styled.div`
-  position: relative;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   padding: 1rem 3rem;
+  > a {
+    text-decoration: none;
+  }
 `
 
-export const Title = styled.h1`
+const Title = styled.h1`
   margin-top: 2rem;
   font-weight: 700;
   text-align: center;
 `
-export const Description = styled.h3`
+const Description = styled.h3`
   margin-top: -1rem;
   font-weight: 400;
   text-align: center;
   color: #7d7d7d;
+`
+
+const ErrorBtn = styled(GlobalButton)`
+  position: relative;
+  margin: 10rem 0;
 `
 
 export default function Error() {
@@ -26,7 +37,7 @@ export default function Error() {
       <Title> 잘못된 경로로 들어오셨어요 </Title>
       <Description>다시 문제를 풀러 가볼까요?</Description>
       <Link to='/'>
-        <Button text='문제 다시 풀러 가기'></Button>
+        <ErrorBtn>문제 풀러 가기</ErrorBtn>
       </Link>
     </Wrapper>
   )
